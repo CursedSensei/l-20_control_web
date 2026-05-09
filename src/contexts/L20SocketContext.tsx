3dddf848a7b29b6613ce046be8ab7e1a5cb6e62e
Zillion <yoshi@area51hirishima.site>
2026-05-10 03:21:51 +0800

    Added support for IOS >=15

--- src/contexts/L20SocketContext.tsx
+++ src/contexts/L20SocketContext.tsx
@@ -99,7 +99,7 @@ export function L20SocketProvider({children}: Readonly<{children: React.ReactNod
     }
 
     function startWebsocket() {
-        const newSocket = new WebSocket("api/ws")
+        const newSocket = new WebSocket("ws://mixer.eloquenceprojects.org/api/ws")
         newSocket.onopen = () => {
             socket.current = newSocket
             switchChannel()
